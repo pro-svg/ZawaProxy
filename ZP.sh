9da2c4f546474be2881e3ba5f8c7f67dbc6f4e26
Me0w <65187274+pro-svg@users.noreply.github.com>
2024-03-07 11:14:26 -0500

    Update ZP.sh

--- ZP.sh
+++ ZP.sh
@@ -3,14 +3,14 @@
 HOTSPOT_NAME="ZawaProxy"
 
 setup_encrypted_dns() {
-    echo "nameserver 1.1.1.1" | su mobile -c "tee /etc/resolv.conf"
+    echo "nameserver 1.1.1.1" | sudo su mobile -c "tee /etc/resolv.conf"
 }
 
 setup_hotspot() {
     echo "Setting up hotspot..."
-    echo "ssid $HOTSPOT_NAME" | su mobile -c "tee /var/preferences/SystemConfiguration/com.apple.wifi.plist"
-    echo "EncryptionType None" | su mobile -c "tee -a /var/preferences/SystemConfiguration/com.apple.wifi.plist"
-    killall CommCenter
+    echo "ssid $HOTSPOT_NAME" | sudo su mobile -c "tee /var/preferences/SystemConfiguration/com.apple.wifi.plist"
+    echo "EncryptionType None" | sudo su mobile -c "tee -a /var/preferences/SystemConfiguration/com.apple.wifi.plist"
+    sudo killall CommCenter
     sleep 5
     echo "Hotspot $HOTSPOT_NAME is now active."
 }
